7e13b9d4cca3274690e2afa15f30524098f0eade
UriBer <ec2-user@ip-172-31-69-171.ec2.internal>
2018-03-24 12:42:12 +0000

    added support for ip address identification

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,9 +1,17 @@
 #!flask/bin/python
 import json
-from flask import Flask, Response
+from flask import Flask, Response, request
 from helloworld.flaskrun import flaskrun
+import requests
 
 application = Flask(__name__)
+
+@application.route('/temp/<temp>', methods=['GET'])
+def get_temp(temp):
+    user_ip = str(request.environ['REMOTE_ADDR'])
+    service_url = 'http://freegeoip.net/json/{}'.format(user_ip) 
+    response = requests.get(service_url).json()
+    return Response(json.dumps({'ip address': '{}'.format(response)}), mimetype='application/json', status=200)
 
 @application.route('/', methods=['GET'])
 def get():
@@ -12,6 +20,7 @@ def get():
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+
 
 if __name__ == '__main__':
     flaskrun(application)
