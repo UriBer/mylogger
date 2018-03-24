38461995fc279dc652e92c1d8df64b24be9ae22f
UriBer <ec2-user@ip-172-31-69-171.ec2.internal>
2018-03-24 20:24:16 +0000

    added tst api

--- helloworld/application.py
+++ helloworld/application.py
@@ -15,6 +15,15 @@ def get_temp(temp):
     return render_template('index.html', title='Stats', response=response) 
     # Response(json.dumps({'ip address': '{}'.format(response)}), mimetype='application/json', status=200)
 
+
+@application.route('/tst/<temp>', methods=['GET'])
+def tst_temp(temp):
+    user_ip = str(request.environ['REMOTE_ADDR'])
+    service_url = 'http://freegeoip.net/json/{}'.format(user_ip) 
+    response = requests.get(service_url).json()
+    #return render_template('index.html', title='Stats', response=response) 
+    return Response(json.dumps({'{}'.format(str(temp)) : '{}'.format(response)}), mimetype='application/json', status=200)
+
 @application.route('/', methods=['GET'])
 def get():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
