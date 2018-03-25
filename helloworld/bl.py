5e0615352a13446413b0f81ab0f1260640429fcb
UriBer <ec2-user@ip-172-31-69-171.ec2.internal>
2018-03-25 06:39:10 +0000

    removing dynmao to see problem with eb

--- helloworld/bl.py
+++ helloworld/bl.py
@@ -7,10 +7,11 @@ import json
 def ip_meta(ip, path):
     service_url = 'http://freegeoip.net/json/{}'.format(ip) 
     response = requests.get(service_url).json()
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('eb_logger_log')
+    #dynamodb = boto3.resource('dynamodb')
+    #table = dynamodb.Table('eb_logger_log')
     res_data = {k: v for k, v in response.items() if v!=''}
     print(res_data)
+    '''
     table.put_item(
     Item={
         'path': path,
@@ -18,6 +19,7 @@ def ip_meta(ip, path):
         'ip_meta' : res_data
          }
     )
+    '''
     #         'ip meta': {k: v for k, v in response.items() if v is not None},
 
     #return render_template('index.html', title='Stats', response=response) 
